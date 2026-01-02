840196641c8d6e117cdc087a1fcfd143e2c7be7b
suicanifuramu <126838878+suicanifuramu@users.noreply.github.com>
2026-01-02 14:45:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from flask import Flask
 import subprocess
 
-result = subprocess.run(["./sys_check"], capture_output=True, text=True)
+app = Flask(__name__)
 
-print(result.stdout)
-
-print(result.stderr)
-
-print(result.returncode)
+@app.route('/')
+def hello_world():
+    result = subprocess.run(["./sys_check"], capture_output=True, text=True)
+    response = f"stdout:\n{result.stdout}\n\nstderr:\n{result.stderr}\n\nreturncode: {result.returncode}"
+    return response
+if __name__ == "__main__":
+    app.run()
